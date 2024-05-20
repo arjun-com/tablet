8162ff44e8a2ad9fc064e47a622d57daa8461c8e
Arjun <rao.d.arjun@gmail.com>
2024-05-20 21:55:17 +0530

    Added support for multiple files.
    1. Changed ordering of command line arguments.
    2. Added multi-file support.

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,25 @@ use std::env;
 use std::process;
 
 fn main() {
-    // Collect command line arguments
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: {} <filename> <spaces_per_tab>", args[0]);
-        process::exit(1);
-    }
+	// Collect command line arguments
+	let args: Vec<String> = env::args().collect();
+	if args.len() < 3 {
+		eprintln!("Usage: {} <spaces_per_tab> <filename x> <filename y> <filename z> <filename ...>", args[0]);
+		process::exit(1);
+	}
 
-    // Parse the number of spaces per tab from the command line argument
-    let spaces_per_tab: usize = args[2].parse().unwrap_or_else(|_| {
-        eprintln!("Invalid number of spaces per tab: {}", args[2]);
-        process::exit(1);
-    });
+	// Parse the number of spaces per tab from the command line argument
+	let spaces_per_tab: usize = args[1].parse().unwrap_or_else(|_| {
+		eprintln!("Invalid number of spaces per tab: {}", args[1]);
+		process::exit(1);
+	});
 
-    // Process the file
-    if let Err(e) = utils::process_file(&args[1], spaces_per_tab) {
-        eprintln!("Error processing file: {}", e);
-        process::exit(1);
-    }
+	// Process files
+
+	for filename in &args[2 .. args.len()] {
+		if let Err(e) = utils::process_file(filename, spaces_per_tab) {
+			eprintln!("Error processing file: {}\nError: {}", filename, e);
+			process::exit(1);
+		}
+	}
 }
--- src/utils.rs
+++ src/utils.rs
@@ -3,21 +3,21 @@ use std::io::{self, BufRead, BufReader, Write};
 use crate::lib;
 
 pub fn process_file(filename: &str, spaces_per_tab: usize) -> io::Result<()> {
-    // Open the file for reading
-    let file = File::open(filename)?;
-    let reader = BufReader::new(file);
+	// Open the file for reading
+	let file = File::open(filename)?;
+	let reader = BufReader::new(file);
 
-    let mut modified_lines = Vec::new();
-    for line in reader.lines() {
-        let line = line?;
-        modified_lines.push(lib::replace_leading_spaces_with_tabs(&line, spaces_per_tab));
-    }
+	let mut modified_lines = Vec::new();
+	for line in reader.lines() {
+		let line = line?;
+		modified_lines.push(lib::replace_leading_spaces_with_tabs(&line, spaces_per_tab));
+	}
 
-    // Open the file for writing and write all modified lines
-    let mut file = File::create(filename)?;
-    for line in modified_lines {
-        writeln!(file, "{}", line)?;
-    }
+	// Open the file for writing and write all modified lines
+	let mut file = File::create(filename)?;
+	for line in modified_lines {
+		writeln!(file, "{}", line)?;
+	}
 
-    Ok(())
+	Ok(())
 }
